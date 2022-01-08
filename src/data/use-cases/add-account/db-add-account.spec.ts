24479b8dc7d259bed1e5b55b34a6aa84a1946af8
Pedro Luís <pfmluis@gmail.com>
2022-01-08 15:14:01 +0100

    test: ensure throw is encryptor throws

--- src/data/use-cases/add-account/db-add-account.spec.ts
+++ src/data/use-cases/add-account/db-add-account.spec.ts
@@ -29,4 +29,16 @@ describe('DbAddAccount', () => {
     await sut.add(accountData)
     expect(encryptorSpy).toHaveBeenCalledWith(accountData.password)
   });
+
+  test('Should throw if encryptor throws', async () => {
+    const { encryptorStub, sut } = makeSut()
+    jest.spyOn(encryptorStub, 'encrypt').mockReturnValueOnce(Promise.reject(new Error()))
+    const accountData = {
+      name: 'Valid Name',
+      email: 'valid_email@mail.com',
+      password: 'validPassword'
+    }
+    const result = sut.add(accountData)
+    await expect(result).rejects.toThrow()
+  });
 });
