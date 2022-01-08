c1fb3f65022e9a2166d4a1f7d083e90040b7ba74
Pedro Luís <pfmluis@gmail.com>
2022-01-08 15:30:21 +0100

    feat: add ensure repository insertion upon add account

--- src/data/use-cases/add-account/db-add-account.ts
+++ src/data/use-cases/add-account/db-add-account.ts
@@ -1,15 +1,19 @@
 import { AccountModel } from '../../../domain/models/account';
 import { AddAccount, AddAccountModel } from '../../../domain/use-cases/add-account';
+import { AddAccountRepository } from '../../protocols/add-account-repository';
 import { Encryptor } from '../../protocols/encryptor';
 
 export class DbAddAccount implements AddAccount {
 
   constructor(
-    private readonly encryptor: Encryptor
+    private readonly encryptor: Encryptor,
+    private readonly addAccountRepository: AddAccountRepository
   ) { }
 
   async add(account: AddAccountModel): Promise<AccountModel> {
-    await this.encryptor.encrypt(account.password)
+    const hashedPassword = await this.encryptor.encrypt(account.password)
+    const accountData = { ...account, password: hashedPassword}
+    await this.addAccountRepository.add(accountData)
     return Promise.resolve(null)
   }
 }
