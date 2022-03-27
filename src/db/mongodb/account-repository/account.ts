345ef4dad473a9fe3de010a658d38e26abedddf7
Pedro Luís <pfmluis@gmail.com>
2022-03-27 22:20:33 +0200

    refactor: move account mapper to separate module

--- src/db/mongodb/account-repository/account.ts
+++ src/db/mongodb/account-repository/account.ts
@@ -2,6 +2,7 @@ import { AddAccountRepository } from '../../../data/protocols/add-account-reposi
 import { AccountModel } from '../../../domain/models/account';
 import { AddAccountModel } from '../../../domain/use-cases/add-account';
 import { MongoHelper } from '../helpers/mongo-helper';
+import { map as accountMapper } from './account-mapper';
 
 export class AccountMongoRepository implements AddAccountRepository {
 
@@ -10,7 +11,6 @@ export class AccountMongoRepository implements AddAccountRepository {
     const result = await accountCollection.insertOne(accountData)
     const account = result.ops[0]
 
-    const { _id, ...accountWithoutId } = account 
-    return Promise.resolve(Object.assign({}, accountWithoutId, { id: _id }))
+    return accountMapper(account)
   }
 }
