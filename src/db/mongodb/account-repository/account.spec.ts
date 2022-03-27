cfbb755d8faa05ae94f1f144d0d4548a1bc49b20
Pedro Luís <pfmluis@gmail.com>
2022-03-27 22:11:09 +0200

    chore: add AddAccount test

---
+++ src/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,31 @@
+import { MongoClient } from 'mongodb';
+import { MongoHelper } from '../helpers/mongo-helper';
+import { AccountMongoRepository } from './account';
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const accountData = {
+      email: 'any_email@email.com',
+      name: 'any_name',
+      password: 'any_password'
+    }
+
+    const account = await sut.add(accountData)
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.email).toBeTruthy()
+    expect(account.name).toBeTruthy()
+    expect(account.password).toBeTruthy()
+    expect(account.email).toBe('any_email@email.com')
+    expect(account.name).toBe('any_name')
+    expect(account.password).toBe('any_password')
+  });
+});
