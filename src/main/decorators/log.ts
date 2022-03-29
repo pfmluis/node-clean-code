f4df6b2ca096563d9d35f58140bd9f1928ce8b9b
Pedro Luís (SWORD Health) <pedroluisswordhealth@MacBook-Air-de-Pedro-5.local>
2022-03-29 10:11:48 +0200

    feat: add logger decorator

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,16 @@
+import { Controller } from "../../presentation/protocols/controller"
+import { HttpRequest } from "../../presentation/protocols/http-request"
+import { HttpResponse } from "../../presentation/protocols/http-response"
+
+export class LogControllerDecorator  implements Controller {
+  constructor (private readonly controller: Controller) {}
+
+  public async handle(request: HttpRequest): Promise<HttpResponse> {
+    const response = await this.controller.handle(request) 
+    if (response.statusCode) {
+      console.error(response.body)
+    }
+
+    return response
+  }
+}
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,15 +1,20 @@
-import { DbAddAccount } from '../../data/use-cases/add-account/db-add-account';
-import { AccountMongoRepository } from '../../db/mongodb/account-repository/account';
 import { BcryptAdapter } from '../../infrastructure/cryptography/bcrypt-adapter';
-import { SignUpController } from '../../presentation/controllers/signup';
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter';
+import { AccountMongoRepository } from '../../db/mongodb/account-repository/account';
+import { DbAddAccount } from '../../data/use-cases/add-account/db-add-account';
+import { SignUpController } from '../../presentation/controllers/signup';
+import { Controller } from '../../presentation/protocols/controller';
+import { LogControllerDecorator } from '../decorators/log';
 
-export const makeSignupController = (): SignUpController => {
+
+export const makeSignupController = (): Controller => {
   const bcryptSalt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(bcryptSalt)
   const accountMongoRepository = new AccountMongoRepository()
   const addAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
 
-  return new SignUpController(emailValidatorAdapter, addAccount)
+  const signupController = new SignUpController(emailValidatorAdapter, addAccount)
+
+  return new LogControllerDecorator(signupController)
 }
