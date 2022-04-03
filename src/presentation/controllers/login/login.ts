a1cd5cd68b6490fe729bf8746014ae6fc61d40e4
Pedro Luís <pedroluis@MacBook-Air-de-Pedro-5.local>
2022-04-03 21:37:04 +0200

    feat: ensure email validator is called

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,17 +1,31 @@
 import { MissingParamError } from '../../errors/missing-param-error';
 import { badRequest } from '../../helpers/http-helpers';
 import { Controller } from '../../protocols/controller';
+import { EmailValidator } from '../../protocols/email-validator';
 import { HttpRequest } from '../../protocols/http-request';
 import { HttpResponse } from '../../protocols/http-response';
 
 export class LoginController implements Controller {
-  public async handle(request: HttpRequest): Promise<HttpResponse> {
 
-    if (!request.body.password) {
+  constructor(private readonly emailValidator: EmailValidator) {
+
+  }
+  public async handle(request: HttpRequest): Promise<HttpResponse> {
+    const { email, password } = request.body
+
+    if (!password) {
       return badRequest(new MissingParamError('password is not defined'))
     }
 
-    return badRequest(new MissingParamError('email is not defined'))
+    if (!email) {
+      return badRequest(new MissingParamError('email is not defined'))
+    }
+
+    if (!this.emailValidator.isValid(email)) {
+      return badRequest(new MissingParamError('email is not valid'))
+    }
+
+    return
   }
   
 }
