9fbe313f729624e9a244482220b9708cd74f42ee
Pedro Luís <pedroluis@MacBook-Air-de-Pedro-5.local>
2022-04-05 09:57:33 +0200

    test: move validators to separate composite

---
+++ src/main/factories/signup-validator.spec.ts
@@ -0,0 +1,17 @@
+import { RequiredFieldValidator } from '../../presentation/helpers/validators/required-field-validator'
+import { ValidationComposite } from '../../presentation/helpers/validators/validator-composite'
+import { makeSignupValidator } from './signup-validator'
+
+jest.mock('../../presentation/helpers/validators/validator-composite')
+
+describe('SignUpValidator Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeSignupValidator()
+    expect(ValidationComposite).toHaveBeenCalledWith([
+      new RequiredFieldValidator('name'),
+      new RequiredFieldValidator('email'),
+      new RequiredFieldValidator('password'),
+      new RequiredFieldValidator('passwordConfirmation')
+    ])
+  })
+})
