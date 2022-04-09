25c9ea83927afc12fd024064d4a1da92cf56d527
Pedro Luís <pedroluis@MacBook-Air-de-Pedro-5.local>
2022-04-09 18:39:33 +0200

    feat: add hash comparer to authenticator

--- src/data/use-cases/authenticator/db-authenticator.ts
+++ src/data/use-cases/authenticator/db-authenticator.ts
@@ -1,12 +1,25 @@
 import { AuthenticationModel, Authenticator } from '../../../domain/use-cases/authenticator';
+import { HashComparer } from '../../protocols/cryptography/hash-comparer';
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email';
 
 export class DbAuthenticator implements Authenticator {
   
-  constructor(private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
+  constructor(
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashComparer: HashComparer
+  ) {}
 
   public async authenticate(auth: AuthenticationModel): Promise<string> {
-    await this.loadAccountByEmailRepository.load(auth.email)
+    const account = await this.loadAccountByEmailRepository.load(auth.email)
+    if (!account) {
+      return null
+    }
+
+    const compareResult = await this.hashComparer.compare(auth.password, account.password)
+    if (!compareResult) {
+      return null
+    }
+
     return null
   }
 }
