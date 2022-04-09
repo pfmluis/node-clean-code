4c45907916cab305bd5879a5d94ddc9ac9450e50
Pedro Luís <pedroluis@MacBook-Air-de-Pedro-5.local>
2022-04-09 18:10:00 +0200

    feat: ensure authenticator calls repo with correct email

---
+++ src/data/use-cases/authenticator/db-authenticator.ts
@@ -0,0 +1,12 @@
+import { AuthenticationModel, Authenticator } from '../../../domain/use-cases/authenticator';
+import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email';
+
+export class DbAuthenticator implements Authenticator {
+  
+  constructor(private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
+
+  public async authenticate(auth: AuthenticationModel): Promise<string> {
+    await this.loadAccountByEmailRepository.load(auth.email)
+    return null
+  }
+}
