ed899ef244934d15741b73c9f086e3397d990eba
Pedro Luís <pedroluis@MacBook-Air-de-Pedro-5.local>
2022-04-10 18:08:49 +0200

    feat: add bcrypt integration

--- src/infrastructure/cryptography/bcrypt-adapter.ts
+++ src/infrastructure/cryptography/bcrypt-adapter.ts
@@ -1,12 +1,17 @@
 import { Hasher } from '../../data/protocols/cryptography/hasher'
 import bcrypt from 'bcrypt'
+import { HashComparer } from '../../data/protocols/cryptography/hash-comparer'
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
   constructor(
     private readonly salt: number
   ) { }
 
   async hash(value: string): Promise<string> {
     return await bcrypt.hash(value, this.salt)
-  }  
+  }
+
+  async compare(value: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(value, hash)
+  }
 }
