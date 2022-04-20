f947aa7f3a1dc2e80bf4e22e13d461bd0b489ef9
Pedro Luís <pedroluis@MacBook-Air-de-Pedro-5.local>
2022-04-20 08:56:56 +0200

    feat: ensure account finds by email

--- src/infrastructure/db/mongodb/account-repository/account-mapper.ts
+++ src/infrastructure/db/mongodb/account-repository/account-mapper.ts
@@ -1,6 +1,10 @@
 import { AccountModel } from '../../../../domain/models/account';
 
 export const map = (account: any): AccountModel => {
+  if (!account) {
+    return null
+  }
+
   const { _id, ...accountWithoutId } = account 
   return Object.assign({}, accountWithoutId, { id: _id })
 }
--- src/infrastructure/db/mongodb/account-repository/account.ts
+++ src/infrastructure/db/mongodb/account-repository/account.ts
@@ -1,10 +1,11 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository';
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository';
 import { AccountModel } from '../../../../domain/models/account';
 import { AddAccountModel } from '../../../../domain/use-cases/add-account';
 import { MongoHelper } from '../helpers/mongo-helper';
 import { map as accountMapper } from './account-mapper';
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
 
   public async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getConnection('accounts')
@@ -12,5 +13,12 @@ export class AccountMongoRepository implements AddAccountRepository {
     const account = result.ops[0]
 
     return accountMapper(account)
+  }
+
+  public async loadByEmail(email: string): Promise<AccountModel> {
+    const connection = await MongoHelper.getConnection('accounts')
+    const result = await connection.findOne({ email })
+
+    return accountMapper(result)
   }
 }
