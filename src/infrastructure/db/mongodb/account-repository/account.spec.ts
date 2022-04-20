8b1b5b217a415c0a3be571257516c796522496fe
Pedro Luís <pedroluis@MacBook-Air-de-Pedro-5.local>
2022-04-20 08:57:11 +0200

    test: ensure account finds by email

--- src/infrastructure/db/mongodb/account-repository/account.spec.ts
+++ src/infrastructure/db/mongodb/account-repository/account.spec.ts
@@ -32,11 +32,33 @@ describe('Account Mongo Repository', () => {
     const account = await sut.add(accountData)
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
-    expect(account.email).toBeTruthy()
-    expect(account.name).toBeTruthy()
-    expect(account.password).toBeTruthy()
     expect(account.email).toBe('any_email@email.com')
     expect(account.name).toBe('any_name')
     expect(account.password).toBe('any_password')
   });
+
+  test('Should return an account on loadByEmail success', async () => {
+    const sut = makeSut()
+    const accountData = {
+      email: 'any_email@email.com',
+      name: 'any_name',
+      password: 'any_password'
+    }
+
+    await (await MongoHelper.getConnection('accounts')).insertOne(accountData)
+
+    const account = await sut.loadByEmail(accountData.email)
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.email).toBe('any_email@email.com')
+    expect(account.name).toBe('any_name')
+    expect(account.password).toBe('any_password')
+  })
+
+
+  test('Should return a null if no account was found', async () => {
+    const sut = makeSut()
+    const account = await sut.loadByEmail("test_email@email.com")
+    expect(account).toBeFalsy()
+  })
 });
