27adae93ba60d09eb94e434a86631b7d625e75a5
Pedro Luís <pedroluis@MacBook-Air-de-Pedro-5.local>
2022-09-26 16:52:18 +0200

    refactor: organize tests by category

--- src/infrastructure/jwt-adapter/jwt-adapter.spec.ts
+++ src/infrastructure/jwt-adapter/jwt-adapter.spec.ts
@@ -12,25 +12,27 @@ const makeSut = (): JwtAdapter => {
 }
 
 describe('Jwt Adapter', () => {
-  test('Should call sign with correct values', async () => {
-    const sut = makeSut()
-    const signSpy = jest.spyOn(jwt, 'sign')
-
-    await sut.encrypt('any_id')
-    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
-  })
-
-  test('Should return result of jwt.sign', async () => {
-    const sut = makeSut()
-    const result = await sut.encrypt('any_id')
-    expect(result).toBe('token')
-  })
-
-  test('Should throw if jwt.sign throws', async () => {
-    const sut = makeSut()
-    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
-    const promise = sut.encrypt('any_id')
-
-    expect(promise).rejects.toThrow()
-  })
+  describe('Encrypt', () => {
+    test('Should call sign with correct values', async () => {
+      const sut = makeSut()
+      const signSpy = jest.spyOn(jwt, 'sign')
+  
+      await sut.encrypt('any_id')
+      expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+    })
+  
+    test('Should return result of jwt.sign', async () => {
+      const sut = makeSut()
+      const result = await sut.encrypt('any_id')
+      expect(result).toBe('token')
+    })
+  
+    test('Should throw if jwt.sign throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+      const promise = sut.encrypt('any_id')
+  
+      expect(promise).rejects.toThrow()
+    })
+  });
 })
