b71426fa6452c0135cc343291260af0e46873ec9
Pedro Luís <pedroluis@MacBook-Air-de-Pedro-5.local>
2022-09-26 09:31:24 +0200

    feat: ensure middleware calls LoadAccountByToken correctly

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,13 +1,57 @@
 import { AccessDeniedError } from '../errors/access-denied-error'
 import { forbidden } from '../helpers/http/http-helpers'
 import { AuthMiddleware } from './auth-middleware'
+import { LoadAccountByToken } from '../../domain/use-cases/load-account-by-token'
+import { AccountModel } from '../../domain/models/account';
 
+type SutTypes = {
+  loadAccountByTokenStub: LoadAccountByToken,
+  sut: AuthMiddleware
+}
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'some_id',
+  email: 'some@email.com',
+  name: 'some_name',
+  password: 'some_password'
+})
+
+const makeLoadAccountByTokenStub = () => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    load(token: string, role?: string | undefined): Promise<AccountModel | null> {
+      return Promise.resolve(makeFakeAccount())
+    }
+  }
+  return new LoadAccountByTokenStub()
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByTokenStub = makeLoadAccountByTokenStub()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
+
+  return {
+     loadAccountByTokenStub,
+     sut,
+  }
+}
 
 describe('Auth Middleware', () => {
   test('should return 403 if no x-access-token exists in headers', async () => {
-    const sut = new AuthMiddleware()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({})
 
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError))
   });
+
+  test('should call LoadAccountByToken with correct access token', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    const loadAccountByTokenSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    await sut.handle({
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    })
+
+    expect(loadAccountByTokenSpy).toHaveBeenCalledWith('any_token')
+  })
 });
