d3ee0d6c309286fd3ad3231140bcd1e0ac62a08f
Pedro Luís <pedroluis@MacBook-Air-de-Pedro-5.local>
2022-09-27 09:10:20 +0200

    feat: make middleware factory

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,10 +1,14 @@
-import { hash } from 'bcrypt'
 import request from 'supertest'
-import { MongoHelper } from '../../infrastructure/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import env from '../config/env'
+import { hash } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+import { MongoHelper } from '../../infrastructure/db/mongodb/helpers/mongo-helper'
 
 
 const surveyCollectionName = 'surveys'
+const accountCollectionName = 'accounts'
+let surveyCollection
 let accountCollection
 
 describe('Survey Routes', () => {
@@ -17,12 +21,14 @@ describe('Survey Routes', () => {
   })
 
   beforeEach(async () => {
-    accountCollection = await MongoHelper.getConnection(surveyCollectionName)
+    surveyCollection = await MongoHelper.getConnection(surveyCollectionName)
+    await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getConnection(accountCollectionName)
     await accountCollection.deleteMany({})
   })
 
   describe('POST /signup', () => {
-    test('Should return 204 on success', async () => {
+    test('Should return 403 on success', async () => {
       await request(app)
         .post('/api/surveys')
         .send({
@@ -37,5 +43,71 @@ describe('Survey Routes', () => {
         })
         .expect(403)
     })
+  })
+
+  test('Should return 403 on access with wrong role', async () => {
+    const password = await hash('valid_password', 12)
+    const result = await accountCollection.insert({
+        email: 'valid.email@email.com',
+        role: 'some_role',
+        password
+    })
+    const id = result.ops[0]._id
+    const accessToken = sign({ id }, env.jwtSecret)
+    await accountCollection.updateOne({
+      _id: id
+    }, {
+      $set: {
+        accessToken
+      }
+    })
+
+    await request(app)
+      .post('/api/surveys')
+      .set('x-access-token', accessToken)
+      .send({
+        question: 'any_question',
+        answers: [{
+          answer: 'any_answer1',
+          image: 'any_image'
+        }, {
+          answer: 'any_answer2',
+          image: 'any_image'
+        }]
+      })
+      .expect(403)
+  })
+
+  test('Should return 204 on success', async () => {
+    const password = await hash('valid_password', 12)
+    const result = await accountCollection.insert({
+        email: 'valid.email@email.com',
+        role: 'admin',
+        password
+    })
+    const id = result.ops[0]._id
+    const accessToken = sign({ id }, env.jwtSecret)
+    await accountCollection.updateOne({
+      _id: id
+    }, {
+      $set: {
+        accessToken
+      }
+    })
+
+    await request(app)
+      .post('/api/surveys')
+      .set('x-access-token', accessToken)
+      .send({
+        question: 'any_question',
+        answers: [{
+          answer: 'any_answer1',
+          image: 'any_image'
+        }, {
+          answer: 'any_answer2',
+          image: 'any_image'
+        }]
+      })
+      .expect(204)
   })
 })
