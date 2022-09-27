c8a3d64a700bedef7c8ffd14d142f8502a714691
Pedro Luís <pedroluis@MacBook-Air-de-Pedro-5.local>
2022-09-27 09:10:20 +0200

    feat: make middleware factory

--- src/main/factories/use-cases/load-account-by-token/load-account-by-token-factory.ts
+++ src/main/factories/use-cases/load-account-by-token/load-account-by-token-factory.ts
@@ -1,10 +1,11 @@
+import env from '../../../config/env'
 import { DbLoadAccountByToken } from '../../../../data/use-cases/load-account-by-token/db-load-account-by-token'
 import { LoadAccountByToken } from '../../../../domain/use-cases/load-account-by-token'
 import { AccountMongoRepository } from '../../../../infrastructure/db/mongodb/account/account-mongo-repository'
 import { JwtAdapter } from '../../../../infrastructure/jwt-adapter/jwt-adapter'
 
 export const makeLoadAccountByToken = (): LoadAccountByToken => {
-  const jwtAdapter = new JwtAdapter(process.env.JWT_SECRET)
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
   const accountMongoRepository = new AccountMongoRepository()
 
   return new DbLoadAccountByToken(jwtAdapter, accountMongoRepository)
